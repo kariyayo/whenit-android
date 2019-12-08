7e1d38cf3681050b6d52666b9fb81dc4aa84a927
bati11 <mail.bati11@gmail.com>
2019-12-08 15:35:24 +0900

    MainActivityからFragmentを使うように変更

---
+++ app/src/main/java/info/bati11/whenit/ui/event/EventFragment.kt
@@ -0,0 +1,25 @@
+package info.bati11.whenit.ui.event
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import info.bati11.whenit.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class EventFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event, container, false)
+    }
+
+}
