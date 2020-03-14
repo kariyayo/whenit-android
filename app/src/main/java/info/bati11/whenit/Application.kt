2b8c89d35aa251587330e9fbb25364a0cdeed5c4
bati11 <mail.bati11@gmail.com>
2020-03-14 20:49:44 +0900

    Flipper導入

--- app/src/main/java/info/bati11/whenit/Application.kt
+++ app/src/main/java/info/bati11/whenit/Application.kt
@@ -1,10 +1,18 @@
 package info.bati11.whenit
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.navigation.NavigationFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.jakewharton.threetenabp.AndroidThreeTen
 import info.bati11.whenit.di.DaggerWhenitAppComponent
 import info.bati11.whenit.di.WhenitAppComponent
 import timber.log.Timber
+
 
 class Application : Application() {
     lateinit var appComponent: WhenitAppComponent
@@ -20,6 +28,14 @@ class Application : Application() {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.start()
         }
     }
 }
