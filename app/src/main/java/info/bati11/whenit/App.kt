4ffeab6d961ce9ca8d3dc93b56958336a3266679
bati11 <mail.bati11@gmail.com>
2020-04-18 23:53:39 +0900

    debugビルトとreleaseビルドでFlipperの初期化処理を分けた

--- app/src/main/java/info/bati11/whenit/App.kt
+++ app/src/main/java/info/bati11/whenit/App.kt
@@ -2,11 +2,6 @@ package info.bati11.whenit
 
 import android.app.Application
 import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -38,13 +33,7 @@ class App : Application(), HasAndroidInjector {
             Timber.plant(Timber.DebugTree())
         }
 
-        SoLoader.init(this, false)
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            val client = AndroidFlipperClient.getInstance(this)
-            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-            client.addPlugin(DatabasesFlipperPlugin(this))
-            client.start()
-        }
+        FlipperInitializer.initFlipper(this)
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
